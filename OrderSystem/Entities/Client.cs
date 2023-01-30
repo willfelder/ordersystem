061e057723417b16e24fbba1e9daf826da5f3ae1
William Lengenfelder <william.lengenfelder@gmail.com>
2023-01-30 07:41:23 -0300

    initial commit

---
+++ OrderSystem/Entities/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return 
+                $"{Name}, " +
+                $"{BirthDate.ToString("MM/dd/yyyy")} - " +
+                $"{Email}";
+        }
+    }
+}
